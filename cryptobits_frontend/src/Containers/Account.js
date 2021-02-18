6a6a247a8331e0810694081b4caedf483dae8c53
Eric Smith <eric.c.smith@sbcglobal.net>
2021-02-18 13:39:19 -0600

    first cut at account page

---
+++ cryptobits_frontend/src/Containers/Account.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+const Account = (props) => {
+
+console.log(props)
+
+return(
+    <div>
+        <tr>
+        <td>Account Name /* props.account.name */</td>
+        <td>Account Email /* props.account.email */</td>
+        <td>Fund Account form goes here </td>
+        <input onChange={(e) => this.setState({cash: e.target.value})} type="cash" name="cash" />
+        </tr>
+    </div>
+)
+
+}
+
+export default Account
