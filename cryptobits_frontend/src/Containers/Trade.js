e7a76249143d3a5c14db78d5d7253071d0bb21a5
Andres-R-Hernandez <andres.hernandez@utexas.edu>
2021-02-19 10:22:34 -0600

    Navbar Logo and Trade Route

---
+++ cryptobits_frontend/src/Containers/Trade.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import TradeForm from '../Components/TradeForm'
+
+export default class Trade extends Component {
+    render() {
+        return (
+            <div>
+                TRADE CONTAINER
+                <TradeForm/>
+            </div>
+        )
+    }
+}
